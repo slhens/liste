0d0f1ae0f563f2eab345296b1e7f9454fdf7901b
slhens <salihenescalik@gmail.com>
2025-04-08 03:59:00 +0300

    v1

--- firebase.js
+++ firebase.js
@@ -1,11 +1,11 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyBN8gSKR2mYomBRSACQLdaRth_qSmb03cg",
-  authDomain: "alisveris-listesi-a8201.firebaseapp.com",
-  projectId: "alisveris-listesi-a8201",
-  storageBucket: "alisveris-listesi-a8201.firebasestorage.app",
-  messagingSenderId: "323343315611",
-  appId: "1:323343315611:web:f6440c4faca65615378c3e",
-  measurementId: "G-JCVZBD44CY"
+  apiKey: "AIzaSyAZAFlNPXKZdlK7qC4DOgkfk1laECodyok",
+  authDomain: "dugunliste.firebaseapp.com",
+  projectId: "dugunliste",
+  storageBucket: "dugunliste.firebasestorage.app",
+  messagingSenderId: "201492782142",
+  appId: "1:201492782142:web:97ff29a0e3497e378c056f",
+  measurementId: "G-GMD68MXZCH"
 };
 
 firebase.initializeApp(firebaseConfig);
